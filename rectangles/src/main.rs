f773d4c8abe9cb7013562f63514b8c00cd151a13
TTcheng <ttchengwang@fomxail.com>
2019-08-05 19:45:03 +0800

    debug annotation on struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
     let rectangle = Rectangle { width: 30, height: 50 };
-
+    println!("rectangle is {:#?}", rectangle);
     println!(
         "The area of the rectangle is {} square pixels.",
         rectangle.area()
     );
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
