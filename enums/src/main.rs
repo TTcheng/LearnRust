34166cdc4582721e7817e5f36899b2adb1c5a288
TTcheng <ttchengwang@fomxail.com>
2019-08-05 19:54:51 +0800

    enum with struct

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -7,3 +7,24 @@ enum IpAddr {
     V4(u8, u8, u8, u8),
     V6(String),
 }
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// 类单元结构体
+struct QuitMessage;
+
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+
+// 元组结构体
+struct WriteMessage(String);
+
+// 元组结构体
+struct ChangeColorMessage(i32, i32, i32); // 元组结构体
