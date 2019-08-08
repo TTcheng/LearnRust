90f7c7900c0e541c193d81f4b92bd3068cb13bb6
TTcheng <ttchengwang@fomxail.com>
2019-08-08 20:17:41 +0800

    panic

---
+++ panic/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{ErrorKind, Read};
+use std::io;
+
+fn main() {
+    let f = match File::open("hello.txt") {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+            },
+            other_error => panic!("There was a problem opening the file: {:?}", other_error),
+        },
+    };
+    // unwrap panic
+    // 成功返回file，失败使用默认panic
+    let file = File::open("hello.txt").unwrap();
+    // 成功返回file，失败使用给出的panic信息
+    let file = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+// 使用？简化错误传递
+fn simple_read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+// 传播错误
+// 将错误返回给代码调用者
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+/*
+// 使用闭包来消灭嵌套match
+fn main() {
+    let f = File::open("hello.txt").map_err(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Tried to create file but there was a problem: {:?}", error);
+            })
+        } else {
+            panic!("There was a problem opening the file: {:?}", error);
+        }
+    });
+}*/
